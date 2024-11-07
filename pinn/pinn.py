7b1940d9cfc74a1533d26acf14ed81f7ea813a1c
nampluskr <nampluskr@gmail.com>
2024-11-08 02:07:38 +0900

    update

--- pinn/pinn.py
+++ pinn/pinn.py
@@ -3,6 +3,42 @@ import numpy as np
 from tqdm import tqdm
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, t, transform=None):
+        self.t = t
+        if t.ndim == 1:
+            self.t = self.t.reshape(-1, 1)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.t)
+
+    def __getitem__(self, idx):
+        t = torch.tensor(self.t[idx]).float()
+        if self.transform:
+            t = self.transform(t)
+        return t
+
+class PINN(nn.Module):
+    def __init__(self, layers_dim=[1, 100, 1], activation="tanh"):
+        super().__init__()
+        functions = {"tanh": nn.Tanh(), "sigmoid": nn.Sigmoid(),
+                     "swish": nn.SiLU(), "silu": nn.SiLU(),
+                     "elu": nn.ELU(), "gelu": nn.GELU(),
+                     "relu": nn.ReLU(), "leakyrelu": nn.LeakyReLU()}
+        layers = []
+        for i in range(len(layers_dim) - 2):
+            layers.append(nn.Linear(layers_dim[i], layers_dim[i+1]))
+            layers.append(functions[activation.lower()])
+        layers.append(nn.Linear(layers_dim[-2], layers_dim[-1]))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
 
 def to_torch(x: np.ndarray):
     tensor = torch.from_numpy(x).float()
@@ -43,7 +79,7 @@ class Trainer:
     def mse_loss(self, x, y):
         return torch.mean((self.model(x) - y)**2)
 
-    def fit(self, t, n_epochs, target=None):
+    def fit(self, t, n_epochs, target=None, scheduler=None):
         self.model.train()
         t = to_torch(t).to(self.device)
 
@@ -71,7 +107,59 @@ class Trainer:
                 loss_total.backward()
                 self.optimizer.step()
 
+                desc = f"Epoch[{epoch}/{n_epochs}] "
+                if scheduler is not None:
+                    desc += f"lr: {scheduler.get_last_lr()[0]:.2e} "
+                    scheduler.step()
+
                 if epoch % 100 == 0:
-                    desc = ' '.join([f"{name}: {values[-1]:.2e}" for name, values in losses.items()])
-                    pbar.set_description(f"Epoch[{epoch}/{n_epochs}] " + desc)
+                    desc += ' '.join([f"{name}: {values[-1]:.2e}" for name, values in losses.items()])
+                    pbar.set_description(desc)
+        return losses
+
+
+class BatchTrainer(Trainer):
+    def fit(self, t, n_epochs, batch_ratio=1.0, target=None, scheduler=None):
+        self.model.train()
+        batch_size = int(len(t) * batch_ratio)
+        batch_loader = DataLoader(Dataset(t), batch_size=batch_size, shuffle=True)
+
+        losses = {"total": [], "eqn": []}
+        if target is not None:
+            for name in target:
+                losses[name] = []
+                target[name][0] = to_torch(target[name][0]).to(self.device)
+                target[name][1] = to_torch(target[name][1]).to(self.device)
+
+        with tqdm(range(1, n_epochs + 1), file=sys.stdout, ascii=True) as pbar:
+            for epoch in pbar:
+                batch_losses = {name: 0 for name in losses}
+                for i, t in enumerate(batch_loader):
+                    t = t.to(self.device)
+                    loss_total = loss_eqn = self.loss_fn(self.model, t)
+                    batch_losses["eqn"] += loss_eqn.item()
+
+                    if target is not None:
+                        for name in target:
+                            loss_data = self.mse_loss(target[name][0], target[name][1])
+                            loss_total += loss_data
+                            batch_losses[name] += loss_data.item()
+
+                    batch_losses["total"] += loss_total.item()
+
+                    self.optimizer.zero_grad()
+                    loss_total.backward()
+                    self.optimizer.step()
+
+                for name in losses:
+                    losses[name].append(batch_losses[name] / (i + 1))
+
+                desc = f"Epoch[{epoch}/{n_epochs}] "
+                if scheduler is not None:
+                    desc += f"(lr: {scheduler.get_last_lr()[0]:.2e}) "
+                    scheduler.step()
+
+                if epoch % 100 == 0:
+                    desc += ' '.join([f"{name}: {values[-1]:.2e}" for name, values in losses.items()])
+                    pbar.set_description(desc)
         return losses
--- pinn/pinn_ivp.py
+++ pinn/pinn_ivp.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from pinn import Trainer, to_torch, to_numpy, gradient, predict
+from pinn import PINN, Trainer, BatchTrainer, gradient, predict
 
 
 def get_model(input_dim, output_dim, hidden_dim=100):
@@ -14,7 +14,6 @@ def get_model(input_dim, output_dim, hidden_dim=100):
         nn.Linear(hidden_dim, output_dim)
     )
     return model
-
 
 
 if __name__ == "__main__":
@@ -27,41 +26,52 @@ if __name__ == "__main__":
     def solution(t):
         return np.sin(2*np.pi*t)/(2*np.pi) + 1
 
+    def eqn(t, u_t):
+        return u_t - torch.cos(2 * np.pi * t)
+
     def loss_fn(model, t):
         t.requires_grad = True
         u = model(t)
         u_t = gradient(u, t)
-        eqn = u_t - torch.cos(2 * np.pi * t)
-        return torch.mean(eqn**2)
+        return torch.mean(eqn(t, u_t)**2)
 
     # Hyperparameters
-    learning_rate = 1e-4
+    learning_rate = 1e-3
     n_epochs = 10000
-    n_samples = 50
+    n_samples = 20
+    layers = [1, 50, 50, 1]
 
     # Model
-    model = get_model(input_dim=1, output_dim=1, hidden_dim=100).to(device)
+    # model = get_model(input_dim=1, output_dim=1, hidden_dim=100).to(device)
+    model = PINN(layers_dim=layers, activation="tanh").to(device)
+    print(model)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.95)
 
     # Data
     t_data = np.linspace(0.25, 1, n_samples) + np.random.randn(n_samples) * 0.1
-    u_data = solution(t_data) +  np.random.randn(n_samples) * 0.1
+    u_data = solution(t_data) +  np.random.randn(n_samples) * 0.01
 
     # Mesh
     t = np.linspace(0, 2, 1001)
-    ones = np.ones_like(t_data)
+    ones = np.ones_like(t)
 
     # target = {"ic": [0*ones, 1*ones]}
     target = {"ic": [0*ones, 1*ones], "data": [t_data, u_data]}
 
-    # Train
-    ivp = Trainer(model, optimizer, loss_fn).init()
-    losses = ivp.fit(t, n_epochs, target=target)
 
-    t = np.linspace(-1, 3, 1001)
+    # Train
+    # ivp = Trainer(model, optimizer, loss_fn).init()
+    # losses = ivp.fit(t, n_epochs, target=target, scheduler=scheduler)
+
+    ivp = BatchTrainer(model, optimizer, loss_fn).init()
+    losses = ivp.fit(t, n_epochs, batch_ratio=0.2, target=target, scheduler=scheduler)
+    
+    # Results
+    # t = np.linspace(-1, 3, 1001)
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 4))
     for name in losses:
-        ax1.semilogy(losses[name], label=name)
+        ax1.semilogy(range(1, n_epochs + 1)[::10], losses[name][::10], label=name)
     ax1.legend(); ax1.set_xlabel("Epoch"); ax1.set_ylabel("Loss")
 
     ax2.plot(t, solution(t), 'k:', label="Exact")
